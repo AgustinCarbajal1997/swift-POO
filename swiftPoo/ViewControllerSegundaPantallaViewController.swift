c62d00d0910285c52b3a6ab3a12c69b368631975
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-10-29 20:20:14 -0300

    [feature] navigation segue

---
+++ swiftPoo/ViewControllerSegundaPantallaViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerSegundaPantallaViewController.swift
+//  swiftPoo
+//
+//  Created by Agustin Carbajal on 29/10/2023.
+//
+
+import UIKit
+
+class ViewControllerSegundaPantallaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
